13841db4815c74160c32c763be9bc17030d2e4fa
priya shanmugha <priyacheatu@gmail.com>
2024-08-06 15:26:33 +0200

    exercice 4 eventListener

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict'
+/*Créez une page HTML avec :
+Un champ de texte pour entrer des tâches.
+Un bouton pour ajouter des tâches à une liste.
+Un mécanisme pour supprimer des tâches de la liste en
+cliquant dessus.
+
+Exercice a publier sur Github avec les parties HTML, CSS et
+javascript
+
+ps : réfléchissez à votre algorithme avant de vous lancer dans
+le code*/
+
